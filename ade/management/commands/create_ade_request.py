dacbaa46f06201e23b23b86fafe617b0ba80fa29
Luca Dutto <duttoluca@gmail.com>
2013-01-17 14:01:07 +0100

    Primo stub di creazione file di richiesta AdE

--- ade/management/commands/create_ade_request.py
+++ ade/management/commands/create_ade_request.py
@@ -6,4 +6,13 @@ class Command(BaseCommand):
     help = "Crea le richieste AdE"
 
     def handle(self, *args, **options):
-        self.stdout.write( "Hello" )
+        ADE_HEADER = "080087670016     REGIONE PIEMONTE                                            ZNLFRZ59L25H620OZANELLA                                 FABRIZIO                                M25071959ROVIGO                                       RO                                                                      A\n"
+        ADE_FOOTER = "980087670016     REGIONE PIEMONTE                                            ZNLFRZ59L25H620OZANELLA                                 FABRIZIO                                M25071959ROVIGO                                       RO          000159000149000008                                          A\n"
+        #self.stdout.write( "Hello" )
+        r_list = ADE_request.objects.filter(status=0)
+        for r in r_list:
+            self.stdout.write(ADE_HEADER)
+            d_list = ADE_detail.objects.filter(ADE_request__id=r.id)
+            for d in d_list:
+                self.stdout.write("1____" + d.cfisc_orig+"\n")
+            self.stdout.write(ADE_FOOTER)

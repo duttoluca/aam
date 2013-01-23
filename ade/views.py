663ee0b4e2a2fe8865a29d5fef322b644ac9c60b
Luca Dutto <duttoluca@gmail.com>
2013-01-23 13:07:54 +0100

    Perfezionato report excel

--- ade/views.py
+++ ade/views.py
@@ -23,25 +23,51 @@ class ADE_requestDetailView(DetailView):
 
 def create_xls(request, pk):
     book = xlwt.Workbook(encoding='utf8')
-    sheet = book.add_sheet('ADE')
-
     default_style = xlwt.Style.default_style
     datetime_style = xlwt.easyxf(num_format_str='dd/mm/yyyy hh:mm')
-    date_style = xlwt.easyxf(num_format_str='dd/mm/yyyy')
 
     req = ADE_request.objects.get(id=pk)
-    values_list = ADE_detail.objects.filter(ADE_request=pk).values_list()
+    details = ADE_detail.objects.filter(ADE_request=req.pk)
 
-    for row, rowdata in enumerate(values_list):
-        for col, val in enumerate(rowdata):
-            if isinstance(val, datetime):
-                style = datetime_style
-            elif isinstance(val, date):
-                style = date_style
-            else:
-                style = default_style
+    sheet = book.add_sheet(req.original_filename)
 
-            sheet.write(row, col, val, style=style)
+    sheet.write(0, 0, 'Dettaglio richiesta AdE', style=default_style)
+    sheet.write(2, 0, 'File originale', style=default_style)
+    sheet.write(3, 0, 'Data inserimento', style=default_style)
+    sheet.write(4, 0, 'Data ritorno', style=default_style)
+    sheet.write(5, 0, 'Stato richiesta', style=default_style)
+
+    sheet.write(2, 1, req.original_filename, style=default_style)
+    sheet.write(3, 1, req.insert_date, style=datetime_style)
+    sheet.write(4, 1, req.return_date, style=datetime_style)
+    sheet.write(5, 1, req.get_status_display(), style=default_style)
+
+    sheet.write(7, 0, 'CFISC ORIGINALE', style=default_style)
+    sheet.write(7, 1, 'CFISC', style=default_style)
+    sheet.write(7, 2, 'COGN/DEN', style=default_style)
+    sheet.write(7, 3, 'NOME/ACRONIMO', style=default_style)
+    sheet.write(7, 4, 'SESSO', style=default_style)
+    sheet.write(7, 5, 'COMUNE RES.', style=default_style)
+    sheet.write(7, 6, 'PROV. RES.', style=default_style)
+    sheet.write(7, 7, 'CAP RES.', style=default_style)
+    sheet.write(7, 8, 'SEDIME RES.', style=default_style)
+    sheet.write(7, 9, 'VIA RES.', style=default_style)
+    sheet.write(7, 10, 'CIVICO RES.', style=default_style)
+    sheet.write(7, 11, 'DATA DECESSO', style=default_style)
+
+    for row, detail in enumerate(details, start=8):
+        sheet.write(row, 0, detail.cfisc_orig, style=default_style)
+        sheet.write(row, 1, detail.cfisc, style=default_style)
+        sheet.write(row, 2, detail.cognome_denominazione, style=default_style)
+        sheet.write(row, 3, detail.nome_acronimo, style=default_style)
+        sheet.write(row, 4, detail.sesso, style=default_style)
+        sheet.write(row, 5, detail.comune_residenza, style=default_style)
+        sheet.write(row, 6, detail.prov_residenza, style=default_style)
+        sheet.write(row, 7, detail.CAP_residenza, style=default_style)
+        sheet.write(row, 8, detail.sedime_residenza, style=default_style)
+        sheet.write(row, 9, detail.nome_via_residenza, style=default_style)
+        sheet.write(row, 10, detail.civico_residenza, style=default_style)
+        sheet.write(row, 11, detail.data_decesso, style=default_style)
 
     response = HttpResponse(mimetype='application/vnd.ms-excel')
     response['Content-Disposition'] = 'attachment; filename=richiesta_' + str(req.pk).rjust(5, '0') + '.xls'

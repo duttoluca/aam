a51da1025f0dccacd201f34fbe42245135adc64e
Luca Dutto <duttoluca@gmail.com>
2013-02-28 14:50:29 +0100

    Aggiunta migrazione

---
+++ ade/migrations/0005_auto__add_field_ade_detail_sogg_valido__add_field_ade_detail_cc1__add_.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'ADE_detail.sogg_valido'
+        db.add_column('ade_ade_detail', 'sogg_valido',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=1, blank=True),
+                      keep_default=False)
+
+        # Adding field 'ADE_detail.cc1'
+        db.add_column('ade_ade_detail', 'cc1',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=1, blank=True),
+                      keep_default=False)
+
+        # Adding field 'ADE_detail.cc2'
+        db.add_column('ade_ade_detail', 'cc2',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=1, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'ADE_detail.sogg_valido'
+        db.delete_column('ade_ade_detail', 'sogg_valido')
+
+        # Deleting field 'ADE_detail.cc1'
+        db.delete_column('ade_ade_detail', 'cc1')
+
+        # Deleting field 'ADE_detail.cc2'
+        db.delete_column('ade_ade_detail', 'cc2')
+
+
+    models = {
+        'ade.ade_detail': {
+            'ADE_request': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['ade.ADE_request']"}),
+            'CAP_residenza': ('django.db.models.fields.CharField', [], {'max_length': '5', 'blank': 'True'}),
+            'Meta': {'ordering': "['ADE_request', 'cfisc_orig']", 'object_name': 'ADE_detail'},
+            'cc1': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'cc2': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'cfisc': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'}),
+            'cfisc_orig': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'civico_residenza': ('django.db.models.fields.CharField', [], {'max_length': '60', 'blank': 'True'}),
+            'cod_belfiore_nascita': ('django.db.models.fields.CharField', [], {'max_length': '4', 'blank': 'True'}),
+            'cod_belfiore_residenza': ('django.db.models.fields.CharField', [], {'max_length': '4', 'blank': 'True'}),
+            'cognome_denominazione': ('django.db.models.fields.CharField', [], {'max_length': '150', 'blank': 'True'}),
+            'comune_nascita': ('django.db.models.fields.CharField', [], {'max_length': '45', 'blank': 'True'}),
+            'comune_residenza': ('django.db.models.fields.CharField', [], {'max_length': '45', 'blank': 'True'}),
+            'data_decesso': ('django.db.models.fields.CharField', [], {'max_length': '8', 'blank': 'True'}),
+            'data_fonte': ('django.db.models.fields.CharField', [], {'max_length': '8', 'blank': 'True'}),
+            'data_nascita': ('django.db.models.fields.CharField', [], {'max_length': '8', 'blank': 'True'}),
+            'fonte': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ind_conf_comune': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'ind_decesso': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'ind_norm': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'nat_giurid': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'nome_acronimo': ('django.db.models.fields.CharField', [], {'max_length': '40', 'blank': 'True'}),
+            'nome_via_residenza': ('django.db.models.fields.CharField', [], {'max_length': '70', 'blank': 'True'}),
+            'prov_nascita': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'prov_residenza': ('django.db.models.fields.CharField', [], {'max_length': '2', 'blank': 'True'}),
+            'sedime_residenza': ('django.db.models.fields.CharField', [], {'max_length': '25', 'blank': 'True'}),
+            'sesso': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'}),
+            'sogg_valido': ('django.db.models.fields.CharField', [], {'max_length': '1', 'blank': 'True'})
+        },
+        'ade.ade_request': {
+            'Meta': {'ordering': "['-insert_date']", 'object_name': 'ADE_request'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'insert_date': ('django.db.models.fields.DateTimeField', [], {}),
+            'original_filename': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'return_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['ade']
